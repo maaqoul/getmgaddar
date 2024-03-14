b3db7e7467a8eb6d32ea19f29be2d3bc9e265c27
Hamza Lemsaoui <hlemsaoui@smb.sqli.com>
2024-03-14 11:25:08 +0000

    refactor auth guard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,32 +1,18 @@
-import { Injectable, inject } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
-import { Observable, map } from 'rxjs';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { map } from 'rxjs';
 import { AuthService } from './auth.service';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class AuthGuard {
-  private readonly router: Router = inject(Router);
-  private authService = inject(AuthService);
-
-  public canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): boolean | Observable<boolean> {
-    return this.authService.getAuthState().pipe(
-      map((state) => {
-        if (!state) {
-          this.router.navigate(['/auth/login']);
-          return false;
-        }
-        return true;
-      })
-    );
-  }
-}
+export const AuthGuard = () => {
+  const router: Router = inject(Router);
+  const authService = inject(AuthService);
+  return authService.getAuthState().pipe(
+    map((state) => {
+      if (!state) {
+        router.navigate(['/auth/login']);
+        return false;
+      }
+      return true;
+    })
+  );
+};
